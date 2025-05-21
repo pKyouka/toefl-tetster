2bdd253eedcf02241368529b2b886dcd64238c32
Pandunanda P. Kafah <pandukyouka@gmail.com>
2025-05-21 14:45:17 +0700

    penambahan result, disable right click, and reading
    need more learn

--- src/main.ts
+++ src/main.ts
@@ -32,4 +32,5 @@ window.addEventListener("DOMContentLoaded", () => {
   readingBtn.type = "button";
   readingBtn.addEventListener("click", goToReading);
   document.body.appendChild(readingBtn);
+  
 });
